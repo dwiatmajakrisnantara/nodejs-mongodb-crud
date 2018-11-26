e07aaf5bd9eb40b4f692cd15eaf6770cf1941521
Md Shayon <mdshayon0@gmail.com>
2018-11-27 04:12:05 +0600

    Here we have completed to create default crud operation,

--- app.js
+++ app.js
@@ -118,6 +118,58 @@ app.post('/articles/add',(req, res)=>{
 
 
 
+//LOAD EDIT FORM
+app.get('/article/edit/:id',(req, res)=>{
+    Article.findById(req.params.id,(err, article)=>{
+        res.render('edit_article', {
+            title: 'Edit title',
+            article: article
+        });
+    });
+});
+
+
+
+
+//UPDATE SUBMIT
+app.post('/articles/edit/:id',(req, res)=>{
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+
+    let query = {_id: req.params.id}
+
+    Article.update(query, article, (err)=>{
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+
+
+
+
+
+app.delete('/article/:id',(req, res)=>{
+    let query ={ _id:req.params.id}
+    Article.remove(query, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send('success');
+    })
+});
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log("server is started on localhost:3000...");
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.delete-article').on('click',function(e){
+        $target = $(e.target);
+        const id = $target.attr('data-id');
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/article/'+id,
+            success: function(response){
+                alert ('delete');
+                window.location.href='/'
+            },
+            error: function(err){
+                console.log(err);
+            }
+        })
+    })
+})
