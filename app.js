92dcf06e5baab780984e1a6a9c358f1b2c5e7e4d
Md Shayon <mdshayon0@gmail.com>
2018-11-27 04:16:17 +0600

    in public folder static file has been removed

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 
 mongoose.connect('mongodb://localhost/nodekb');
@@ -39,6 +42,44 @@ app.use(bodyParser.json());
 
 //SET PUBLIC FOLDER FOR STATIC ASSETS
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+//EXPRESS SESSION MIDDLEWARE
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUnintialized: true,
+    cookie: { secure: true }
+}));
+
+
+
+//EXPRESS MESSAGES MIDDLEWARE
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//EXPRESS VALIDATOR MIDDLEWARE
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        let namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+        while(namespace.length){
+            formParam += '['+namespace.shift()+ ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value:value
+        }
+    }
+}));
+
 
 
 
